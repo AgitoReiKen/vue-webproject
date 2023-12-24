cb748e46bcd25d741eb236f55dec11f3cf4e217c
agitoreiken <agitokun1@gmail.com>
2023-12-24 20:28:12 +0900

    updated profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,31 +7,32 @@
       <img
         class="avatar-large"
         alt="avatar"
-        :src="steamUser['avatarfull']"
+        :src="auth.AvatarUrl"
         style="outline: 0.5rem solid var(--c-dark-3); width: 12rem"
       />
     </div>
     <div class="widget bg-dark-3" style="min-width: 16rem; justify-content: center">
-      <p class="text-m bold">{{ steamUser['personaname'] }}</p>
+      <p class="text-m bold">{{ auth.Name }}</p>
     </div>
     <XInput
       type="text"
       lclass="text-s c-light-2"
       iclass="text-s c-light-2 padding-s"
       label="SteamID"
-      v-model="steamId"
-      readonly="true"
+      v-model="auth.SteamId"
+      :readonly="true"
     ></XInput>
     <XInput
       type="text"
       lclass="text-s c-light-2"
       iclass="text-s c-light-2 padding-s"
-      label="Discord [NOT ATTACHED]"
-      v-model="discordId"
-      readonly="true"
+      label="Discord"
+      v-model="auth.DiscordId"
+      :readonly="true"
     ></XInput>
     <div class="column gap-l">
       <XButton
+        v-if="!hasDiscord"
         class="primary bg-purple-2 glow text-m shiny text-uppercase"
         @click="onConnectDiscord"
       >
@@ -41,6 +42,7 @@
         <template v-slot:append>Connect discord</template>
       </XButton>
       <XButton
+        v-if="!hasAsa"
         class="primary bg-green-2 glow text-m shiny text-uppercase"
         style="width: 100%"
         @click="onConnectArk"
@@ -65,22 +67,30 @@ import XInput from '@/components/XInput.vue'
 import XButton from '@/components/XButton.vue'
 import IconDiscord from '@/components/icons/IconDiscord.vue'
 import IconArk from '@/components/icons/IconArk.vue'
+import { useAuthStore } from '@/stores/auth'
 
 export default {
   components: { IconArk, IconDiscord, XInput, XButton },
   name: 'ProfileView',
-  setup() {},
-  mounted() {
-    this.steamSign = this.$cookies.get('steamsign')
-    this.steamUser = this.$cookies.get('steamuser')
-    this.steamId = this.steamUser['steamid']
+  setup() {
+    const auth = useAuthStore()
+    return {
+      auth: auth.data
+    }
   },
   data() {
-    return {
-      steamId: '',
-      discordId: '',
-      steamUser: '',
-      steamSign: ''
+    return {}
+  },
+  computed: {
+    hasAsa: {
+      get() {
+        return this.auth.AsaEosId.length !== 0
+      }
+    },
+    hasDiscord: {
+      get() {
+        return this.auth.DiscordId.length !== 0
+      }
     }
   },
   methods: {
